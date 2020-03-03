6e0e4a98d6981e797038711ce3c7f690dc30c2a3
김용후 <yhkim13@cyberlogitec.com>
2020-03-03 16:20:47 +0900

    add /auth/check get request

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -23,56 +23,7 @@ router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/auth');
 });
-/*
-router.get('/check', function (req, res){
-    // read the token from header or url 
-    const token = req.headers['x-access-token'] || req.query.token
-
-    // token does not exist
-    if(!token) {
-        return res.status(403).json({
-            success: false,
-            message: 'not logged in'
-        })
-    }
-
-    // create a promise that decodes the token
-    const p = new Promise(
-        (resolve, reject) => {
-            jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-                if(err) reject(err)
-                resolve(decoded)
-            })
-        }
-    )
-
-    // if token is valid, it will respond with its info
-    const respond = (token) => {
-        res.json({
-            success: true,
-            info: token
-        })
-    }
-
-    // if it has failed to verify, it will return an error message
-    const onError = (error) => {
-        res.status(403).json({
-            success: false,
-            message: error.message
-        })
-    }
-
-    // process the promise
-    p.then(respond).catch(onError)
-});
-*/
-router.get('/check', passport.authenticate('jwt-check',{
-    successRedirect: '/auth/check/success',
-    failureRedirect: '/auth/check/failed',
-    failureFlash: true
-}));
-router.get('/check/success',function(req, res){
-    console.log('hi');
-
+router.get('/check', passport.authenticate('jwt', {session: false}), async (req, res)=>{
+    res.send('success');
 });
 module.exports = router;
