6f0f5197e9f9fdb48f8524ce5c1c57b7890034ab
김용후 <yhkim13@cyberlogitec.com>
2020-03-12 17:39:29 +0900

    seperate signin router and authcheck router

--- routes/authRouter.js
+++ routes/authRouter.js
@@ -3,41 +3,40 @@ const passport = require('passport');
 const jwt = require("jsonwebtoken");
 const router = express.Router();
 
-router.get('/success', function(req, res){
+router.get('/signInSuccess', function(req, res){
     const token = jwt.sign(req.user.email, process.env.JWT_SECRET);
     const user = req.user.email;
     const status = true;
     return res.json({status, user, token});
 
 });
-router.get('/failed',function(req, res){
+router.get('/signInfailed',function(req, res){
     const status = false;
     const message = req.flash('signinMessage')[0];
     return res.json({status, message});
 });
-
+router.get('/checkFailed',function(req, res){
+    return res.status(401).send({
+        "error": {
+            "code": "401",
+            "message": "Authorization failed\nPlease login first"
+        }
+    });
+});
+router.get('/checkSuccess',function(req, res){
+    return res.send('success');
+});
 router.post('/signin', passport.authenticate('local-signin', {
-    successRedirect: '/auth/success',
-    failureRedirect: '/auth/failed',
+    successRedirect: '/auth/signInSuccess',
+    failureRedirect: '/auth/signInfailed',
     failureFlash: true
 }));
-
+router.get('/check', passport.authenticate('jwt', {
+    successRedirect: '/auth/checkSuccess',
+    failureRedirect: '/auth/checkFailed',
+}));
 router.get('/logout', function (req, res) {
     req.logout();
     res.redirect('/auth');
 });
-router.get('/check', function authenticateJwt(req, res, next) {
-    passport.authenticate('jwt', function (err, user, info) {
-        if (err) return next(err);
-        if (!user) return res.status(401).send({
-            "error": {
-                "code": "401",
-                "message": "Authorization failed\nPlease login first"
-            }
-        });
-        req.user = user;
-        next();
-    })(req, res, next);
-});
-
 module.exports = router;
